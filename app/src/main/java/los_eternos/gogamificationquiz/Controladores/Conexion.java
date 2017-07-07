2eaee73389b3bb00c90d220089bd707f57635f91
Rodrigo <rodrigoxj32@hotmail.com>
2017-07-07 16:31:34 -0600

    recuperacion de perfil

---
+++ app/src/main/java/los_eternos/gogamificationquiz/Controladores/Conexion.java
@@ -0,0 +1,16 @@
+package los_eternos.gogamificationquiz.Controladores;
+
+/**
+ * Created by rodrigo on 07-07-17.
+ */
+
+public class Conexion {
+
+    public String URLLocal = "http://192.168.1.6:8000/";
+
+    public Conexion() {}
+
+    public String getURLLocal() {
+        return URLLocal;
+    }
+}
--- app/src/main/java/los_eternos/gogamificationquiz/Pantallas/PerfilFragment.java
+++ app/src/main/java/los_eternos/gogamificationquiz/Pantallas/PerfilFragment.java
@@ -1,11 +1,22 @@
 package los_eternos.gogamificationquiz.Pantallas;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import los_eternos.gogamificationquiz.Controladores.Conexion;
+import los_eternos.gogamificationquiz.Controladores.ControlServicio;
+import los_eternos.gogamificationquiz.Modelo.Perfil;
 import los_eternos.gogamificationquiz.R;
 
 /**
@@ -15,6 +26,48 @@ import los_eternos.gogamificationquiz.R;
 public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.perfil_fragment, null);
+
+        //codigo de conexion
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        View view = inflater.inflate(R.layout.perfil_fragment, container,false);
+
+        TextView nombre = (TextView) view.findViewById(R.id.nombrePerfil);
+        ImageView foto = (ImageView) view.findViewById(R.id.fotoPerfil);
+        TextView descripcion = (TextView) view.findViewById(R.id.descripcionPerfil);
+
+        Conexion con = new Conexion();
+        String url="";
+
+        url += con.getURLLocal()+"perfil/1";
+
+        List<Perfil> perfil = null;
+
+        perfil = ControlServicio.obtenerPerfil(url, getActivity());
+
+
+        for (int i =0; i<perfil.size();i++){
+           nombre.setText(perfil.get(i).getNombrePerfil());
+
+            byte[] decodedByte = Base64.decode(perfil.get(i).getImagenPerfil(), 0);
+
+            foto.setImageBitmap(BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length));
+            String des = perfil.get(i).getDescripcionPerfil();
+
+
+            if(des.compareTo("null") == 0){
+
+            }else {
+
+                descripcion.setText(perfil.get(i).getDescripcionPerfil());
+            }
+
+
+        }
+
+
+
+
+        return view;
     }
 }
