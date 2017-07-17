faf229a415a63ce1a8de207d3f3fb6972f243060
Alam Lopez <alam.lqnpa6@hotmail.com>
2017-07-17 02:54:38 -0600

    Arreglos menores

--- app/src/main/java/los_eternos/gogamificationquiz/Pantallas/MateriasExistentesFragment.java
+++ app/src/main/java/los_eternos/gogamificationquiz/Pantallas/MateriasExistentesFragment.java
@@ -6,26 +6,19 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-
-import los_eternos.gogamificationquiz.Modelo.Docente;
-import los_eternos.gogamificationquiz.Modelo.Materia;
-
 import los_eternos.gogamificationquiz.R;
+
 
 /**
  * Created by Hunter on 12/7/2017.
@@ -41,8 +34,8 @@ public class MateriasExistentesFragment extends Fragment{
     public ArrayList<String> codigoCiclo;
     public ArrayList<String> idMateria;
     public ArrayList<String> idGrupo;
-    public String email;
-    public int resultado;
+    public static String email;
+    public static int resultado;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -56,8 +49,8 @@ public class MateriasExistentesFragment extends Fragment{
         ArrayList<String> codiCiclo = null;
         ArrayList<String> idMate = null;
         ArrayList<String> idGru = null;
-        String correo;
-        int result;
+        String correo="";
+        int result=0;
 
         //Recibe los parametros de la Activity
         if (getArguments() != null) {
@@ -83,9 +76,16 @@ public class MateriasExistentesFragment extends Fragment{
         email = correo;
         resultado = result;
 
+
+        System.out.println(codigoCiclo);
+        System.out.println(idGrupo);
+        System.out.println(idMateria);
+        System.out.println(email);
+        System.out.println(resultado);
+
         //Enlazando la activity con el RecyclerView
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.recycler_view, container, false);
-        MateriasExistentesFragment.ContentAdapter adapter = new ContentAdapter(recyclerView.getContext(), nombreMateria, codigoMateria, nombreGrupo, codigoCiclo, imagenMateria);
+        MateriasExistentesFragment.ContentAdapter adapter = new ContentAdapter(recyclerView.getContext(), nombreMateria, codigoMateria, nombreGrupo, codigoCiclo, idMateria, idGrupo, imagenMateria);
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
@@ -102,11 +102,13 @@ public class MateriasExistentesFragment extends Fragment{
         public TextView codmateria;
         public TextView nombgrupo;
         public TextView codciclo;
+        public TextView idmateria;
+        public TextView idgrupo;
         public ImageView imagmateria;
         public RelativeLayout relative;
-        private final Context context;
-        MateriasExistentesFragment mef = new MateriasExistentesFragment(); //Instancia de la clase externa
 
+        private final Context context;
+        //MateriasExistentesFragment mef = new MateriasExistentesFragment(); //Instancia de la clase externa
 
         public MateriaViewHolder(LayoutInflater inflater, ViewGroup parent) {
 
@@ -117,9 +119,13 @@ public class MateriasExistentesFragment extends Fragment{
             codmateria = (TextView) itemView.findViewById(R.id.materia_codigo);
             nombgrupo = (TextView) itemView.findViewById(R.id.grupo_nombre);
             codciclo = (TextView) itemView.findViewById(R.id.codigo_ciclo);
+            idmateria = (TextView) itemView.findViewById(R.id.id_materia);
+            idgrupo = (TextView) itemView.findViewById(R.id.id_grupo);
             imagmateria = (ImageView) itemView.findViewById(R.id.materia_imagen);
             relative = (RelativeLayout) itemView.findViewById(R.id.main_content);
             context = itemView.getContext();
+            //otro = codciclo.getText().toString();
+
 
             //Evento que se activa al presionar un item del RecyclerView
             itemView.setOnClickListener(new View.OnClickListener() {
@@ -127,14 +133,20 @@ public class MateriasExistentesFragment extends Fragment{
                 public void onClick(View view) {
 
                     //Si el que esta registrado es estudiante
-                    if(mef.resultado == 1){
+                    if(resultado == 1){
+
+                        System.out.println("Resultado: " + resultado);
+                        System.out.println("Id materia: " + idmateria.getText().toString());
+                        System.out.println("Id grupo: " + idgrupo.getText().toString());
+                        System.out.println("Codigo ciclo: " + codciclo.getText().toString());
+                        System.out.println("Email: " + email);
 
                         Intent intent = new Intent(context.getApplicationContext(), EstudianteActivity.class);
-                        intent.putExtra("idmateria", mef.idMateria);
-                        intent.putExtra("idgrupo", mef.idGrupo);
-                        intent.putExtra("email", mef.email);
-                        intent.putExtra("codigociclo", mef.codigoCiclo);
-                        intent.putExtra("resultado", mef.resultado);
+                        intent.putExtra("idmateria", idmateria.getText().toString());
+                        intent.putExtra("idgrupo", idgrupo.getText().toString());
+                        intent.putExtra("email", email);
+                        intent.putExtra("codigociclo", codciclo.getText().toString());
+                        intent.putExtra("resultado", resultado);
                         context.startActivity(intent);
 
                     }
@@ -142,12 +154,18 @@ public class MateriasExistentesFragment extends Fragment{
                     //Si el que esta registrado es docente
                     else {
 
+                        System.out.println("Resultado: " + resultado);
+                        System.out.println("Id materia: " + idmateria.getText().toString());
+                        System.out.println("Id grupo: " + idgrupo.getText().toString());
+                        System.out.println("Codigo ciclo: " +  codciclo.getText().toString() );
+                        System.out.println("Email: " + email);
+
                         Intent intent = new Intent(context.getApplicationContext(), DocenteActivity.class);
-                        intent.putExtra("idmateria", mef.idMateria);
-                        intent.putExtra("idgrupo", mef.idGrupo);
-                        intent.putExtra("email", mef.email);
-                        intent.putExtra("codigociclo", mef.codigoCiclo);
-                        intent.putExtra("resultado", mef.resultado);
+                        intent.putExtra("idmateria", idmateria.getText().toString());
+                        intent.putExtra("idgrupo", idgrupo.getText().toString());
+                        intent.putExtra("email", email);
+                        intent.putExtra("codigociclo", codciclo.getText().toString());
+                        intent.putExtra("resultado", resultado);
                         context.startActivity(intent);
 
                     }
@@ -167,10 +185,12 @@ public class MateriasExistentesFragment extends Fragment{
 
 
         private int LENGTH;
-        private  String[] nombreMaterias ;
-        private  String[] codigoMaterias ;
-        private  String[] nombreGrupos ;
-        private  String[] imagenMaterias;
+        private String[] nombreMaterias ;
+        private String[] codigoMaterias ;
+        private String[] nombreGrupos ;
+        private String[] idMaterias;
+        private String[] idGrupos;
+        private String[] imagenMaterias;
         private String[] codigoCiclos;
         Context context;
          //private String[] mPlaceDesc;
@@ -178,7 +198,7 @@ public class MateriasExistentesFragment extends Fragment{
 
 
 
-        public ContentAdapter(Context context, ArrayList<String> nom, ArrayList<String> cod, ArrayList<String> gru, ArrayList<String> cic, ArrayList<String> ima) {
+        public ContentAdapter(Context context, ArrayList<String> nom, ArrayList<String> cod, ArrayList<String> gru, ArrayList<String> cic, ArrayList<String> idm, ArrayList<String> idg, ArrayList<String> ima) {
             Resources resources = context.getResources();
 
             this.context = context;
@@ -187,10 +207,14 @@ public class MateriasExistentesFragment extends Fragment{
             nombreMaterias = new String[nom.size()];
             nombreMaterias = nom.toArray(nombreMaterias);
 
-
             codigoMaterias = new String[cod.size()];
             codigoMaterias = cod.toArray(codigoMaterias);
 
+            idMaterias = new String[idm.size()];
+            idMaterias = idm.toArray(idMaterias);
+
+            idGrupos = new String[idg.size()];
+            idGrupos = idg.toArray(idGrupos);
 
             nombreGrupos = new String[gru.size()];
             nombreGrupos = gru.toArray(nombreGrupos);
@@ -219,6 +243,8 @@ public class MateriasExistentesFragment extends Fragment{
             holder.codmateria.setText(codigoMaterias[position % codigoMaterias.length]);
             holder.codciclo.setText(codigoCiclos[position % codigoCiclos.length]);
             holder.nombgrupo.setText(nombreGrupos[position % nombreGrupos.length]);
+            holder.idmateria.setText(idMaterias[position % idMaterias.length]);
+            holder.idgrupo.setText(idGrupos[position % idGrupos.length]);
             //holder.mano.setTag(R.id.TAG_COLOR_ID,"gray");
             //holder.punto.setTag(R.id.TAG_COLOR_ID,"gray");
 
